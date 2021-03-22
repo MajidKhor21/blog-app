d845b2c371d5a960cb65269ffc3dbc7f23b47e40
Majid Khorshidian <mkhorshidian72@gmail.com>
2021-03-22 21:23:02 +0430

    add user information route and ui

--- routes/user.js
+++ routes/user.js
@@ -8,4 +8,19 @@ router.get("/dashboard", checker.loginChecker, (req, res) => {
   res.render("dashboard", { user: req.session.user });
 });
 
+router.get("/info", checker.loginChecker, (req, res) => {
+  if (req.session.user.gender === "male") {
+    req.session.user.gender = "مرد";
+  } else if (req.session.user.gender === "female") {
+    req.session.user.gender = "زن";
+  }
+  if (req.session.user.role === "admin") {
+    req.session.user.role = "مدیر";
+  } else if (req.session.user.role === "blogger") {
+    req.session.user.role = "نویسنده";
+  }
+  console.log(req.session.user.gender);
+  res.render("user-info", { user: req.session.user });
+});
+
 module.exports = router;
