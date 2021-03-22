b9ecef6ea17f919eb6e278526903ee4147530135
Majid Khorshidian <mkhorshidian72@gmail.com>
2021-03-22 13:57:01 +0430

    setup mongoodb

--- app.js
+++ app.js
@@ -3,8 +3,16 @@ const express = require("express");
 const path = require("path");
 const cookieParser = require("cookie-parser");
 const logger = require("morgan");
+const mongoose = require("mongoose");
+const config = require("./config/config");
 
 const app = express();
+
+// setup mongoose
+mongoose.connect(config.mongoUrl, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+});
 
 // view engine setup
 app.set("views", path.join(__dirname, "views"));
