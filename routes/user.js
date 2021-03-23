016eb9975407d7686aae722fd74fde7f2c033bfe
Majid Khorshidian <mkhorshidian72@gmail.com>
2021-03-23 10:16:27 +0430

    first introduction for update user

--- routes/user.js
+++ routes/user.js
@@ -9,18 +9,11 @@ router.get("/dashboard", checker.loginChecker, (req, res) => {
 });
 
 router.get("/info", checker.loginChecker, (req, res) => {
-  if (req.session.user.gender === "male") {
-    req.session.user.gender = "مرد";
-  } else if (req.session.user.gender === "female") {
-    req.session.user.gender = "زن";
-  }
-  if (req.session.user.role === "admin") {
-    req.session.user.role = "مدیر";
-  } else if (req.session.user.role === "blogger") {
-    req.session.user.role = "نویسنده";
-  }
-  console.log(req.session.user.gender);
   res.render("user-info", { user: req.session.user });
+});
+
+router.get("/edit", checker.loginChecker, (req, res) => {
+  res.render("user-edit", { user: req.session.user });
 });
 
 module.exports = router;
